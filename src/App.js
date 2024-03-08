54dbbe508694673c226f5431e66793f8f61a90ed
kingboseong <rlaqhtjd8001@naver.com>
2024-03-08 18:10:52 +0900

    03_08

--- src/App.js
+++ src/App.js
@@ -2,19 +2,19 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
+  //Fragment태그 <></>
+  <> 
+    return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           k-digital 6기 김보성
         </p>
-        <a>
-          
-        </a>
       </header>
     </div>
-  );
+    );
+  </>
 }
 
 export default App;
